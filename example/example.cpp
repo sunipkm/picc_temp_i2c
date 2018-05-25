fd6c6f643c366fd355b54c0aab7eeeb6c80b45d2
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2018-05-25 15:05:35 -0400

    Example Tweaks

--- example/example.cpp
+++ example/example.cpp
@@ -1,17 +1,22 @@
 #include <picc_temp_i2c.h>
 #include <iostream>
+#include <iomanip>
+
 using namespace std ;
 
 int main ( void )
 {
 	hdc1010 dev1 , dev2 ;
-	dev1.begin(0x41) ; dev2.begin(0x43) ;
+	dev1.begin(0x41) ; dev2.begin(0x43) ; //two devices
+
+	cout << fixed << setprecision(3) ;
 
 	cout << "Mf ID Dev1:0x" << hex << dev1.readMfId()  << endl ;
 	cout << "Mf ID Dev2: 0x" << hex << dev2.readMfId()  << endl ;
 	uint64_t time = 0 ;
 	while(true)
-	{	cout << "At " << dec << ++time << " seconds:" << endl ;
+	{	
+		cout << "At " << dec << ++time << " seconds:" << endl ;
 		cout << "Temp Dev1: " << dev1.readT() << " C" << endl ;
 		cout << "Temp Dev2: " << dev2.readT() << " C" << endl ;
 		cout << endl ;
@@ -19,6 +24,13 @@ int main ( void )
 		cout << "RH Dev2: " << dev2.readH() << " %" << endl ;
 		cout << endl ;
 		hdc1010::sleep(1000) ; //spit out numbers in 1 second intervals
+		cout << "x1b[A" ;
+		cout << "x1b[A" ;
+		cout << "x1b[A" ;
+		cout << "x1b[A" ;
+		cout << "x1b[A" ;
+		cout << "x1b[A" ;
+		cout << "x1b[A" ;
 	}
 	return 0 ;
 }
