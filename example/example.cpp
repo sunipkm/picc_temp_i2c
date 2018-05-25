97acf2a8c38adb6892aa2c9a345b83ecfc61fd92
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2018-05-25 14:46:14 -0400

    Example Tweaked

--- example/example.cpp
+++ example/example.cpp
@@ -7,15 +7,17 @@ int main ( void )
 	hdc1010 dev1 , dev2 ;
 	dev1.begin(0x41) ; dev2.begin(0x43) ;
 
-	cout << "Mf ID Dev1: " << hex << dev1.readMfId()  << endl ;
-	cout << "Mf ID Dev2: " << hex << dev2.readMfId()  << endl ;
-
+	cout << "Mf ID Dev1:0x" << hex << dev1.readMfId()  << endl ;
+	cout << "Mf ID Dev2: 0x" << hex << dev2.readMfId()  << endl ;
+	uint64_t time = 0 ;
 	while(true)
-	{
+	{	cout << "At " << ++time << " seconds:" << endl ;
 		cout << "Temp Dev1: " << dev1.readT() << " C" << endl ;
 		cout << "Temp Dev2: " << dev2.readT() << " C" << endl ;
+		cout << endl ;
 		cout << "RH Dev1: " << dev1.readH() << " %" << endl ;
 		cout << "RH Dev2: " << dev2.readH() << " %" << endl ;
+		cout << endl ;
 		hdc1010::sleep(1000) ; //spit out numbers in 1 second intervals
 	}
 	return 0 ;
