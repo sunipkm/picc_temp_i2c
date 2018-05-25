851b684ce064cf083362457a0d8f92ab68d612b9
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2018-05-24 23:40:57 -0400

    Version 1

---
+++ include/picc_temp_i2c.h
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <errno.h>
+
+//HDC1010, big-endian device
+
+#ifndef PICC_TEMP_I2C
+#define PICC_TEMP_I2C
+
+#ifndef I2C_FILE
+#define I2C_FILE "/dev/i2c-1"
+
+#ifndef I2C_ADDR
+#define I2C_ADDR 0x40
+#endif
+
+#define TRUE 1
+#define FALSE 0
+
+typedef enum { //registers available
+	TEMPERATURE = 0x00,
+	HUMIDITY = 0x01,
+	CONFIGURATION = 0x02,
+	MANUFACTURER_ID = 0xFE,
+	DEVICE_ID = 0xFF,
+	SERIAL_ID_FIRST = 0xFB,
+	SERIAL_ID_MID = 0xFC,
+	SERIAL_ID_LAST = 0xFD,
+} hdc1010_ptrs ;
+
+typedef union {
+	uint8_t rawData ;
+	struct {
+		uint8_t HumidityMeasurementResolution : 2;
+		uint8_t TemperatureMeasurementResolution : 1;
+		uint8_t BatteryStatus : 1;
+		uint8_t ModeOfAcquisition : 1;
+		uint8_t Heater : 1;
+		uint8_t ReservedAgain : 1;
+		uint8_t SoftwareReset : 1;
+	};
+} hdc1010_regs ;
+
+class hdc1010 {
+public:
+	hdc1010();
+
+	bool begin(uint8_t address);
+	uint16_t readMfId(); // 0x5449 ID of Texas Instruments
+	uint16_t readDevId(); // 0x1000 ID of the device
+
+	HDC1010_Registers readReg();	
+	void writeReg(HDC1010_Registers reg);
+
+	void heatUp(uint8_t seconds);
+
+	float readT();
+	float readH();
+
+
+private:
+	uint8_t _address;
+	uint16_t readData(uint8_t pointer);
+	void writeData(uint8_t val) ;
+	void readBytes(uint8_t * buf , uint8_t n ) ;
+
+	int i2cdevbus ;
+
+};
+
+
+const double PICC_POW16 = 65536.0 ;
+
+
+
+#endif
---
+++ src/picc_temp_i2c.c
@@ -0,0 +1,110 @@
+#include <picc_temp_i2c.h>
+
+hdc1010::hdc1010() {
+	return ;
+}
+
+
+
+bool hdc1010::begin ( uint8_t address )
+{
+	_address = address ;
+	
+	i2cdevbus = open(I2C_FILE, O_RDWR) ;
+
+	if ( i2cdevbus < 0 )
+	{
+		printf ( "FILE %s Line %d: Error opening device %s, %s.\n" , __FILE__ , __LINE__ , I2C_FILE , strerror ( errno ) ) ;
+		return false ;
+	}
+
+	if ( ioctl ( i2cdevbus , I2C_SLAVE, address ) < 0 ) 
+	{
+		printf ( "FILE %s Line %d: ioctl error: %s\n", __FILE__ , __LINE__ , strerror(errno) ) ;
+		return false ;
+	}
+
+	writeData(CONFIGURATION) ; //write to the configuration register
+	writeData(0x00) ; //rawData
+	writeData(0x00) ; //flags
+
+	/* config: default.
+	Heater: Off
+	Mode: T or RH
+	T: 14 bits
+	RH: 14 bits
+	*/
+
+	return true ;
+}
+
+float hdc1010::readT()
+{
+	uint16_t rawT = readData(TEMPERATURE);
+	return (rawT/PICC_POW16) * 165 - 40 ;
+}
+
+float hdc1010::readH() {
+	uint16_t rawH = readData(HUMIDITY);
+	return (rawH / PICC_POW16) * 100;
+}
+
+uint16_t hdc1010::readMfId() {
+	return readData(MANUFACTURER_ID);
+}
+
+hdc1010_regs hdc1010::readRegister() {
+	hdc1010_Registers reg;
+	reg.rawData = (readData(CONFIGURATION) >> 8);
+	return reg;
+}
+
+void hdc1010::writeRegister(hdc1010_regs reg) {
+	writeData(CONFIGURATION);
+	writeData(reg.rawData);
+	writeData(0x00);
+	usleep(10000);
+}
+
+void hdc1010::heatUp(uint8_t seconds) {
+	hdc1010_regs reg = readRegister();
+	reg.Heater = 1;
+	reg.ModeOfAcquisition = 1;
+	writeRegister(reg);
+
+	uint8_t buf[4];
+	for (int i = 1; i < (seconds*66); i++) {
+		writeData(0x00);
+		delay(20000);
+		readBytes(buf,4) ;
+	}
+	reg.Heater = 0;
+	reg.ModeOfAcquisition = 0;
+	writeRegister(reg);
+}
+
+uint16_t hdc1010::readDevId() {
+	return readData(DEVICE_ID);
+}
+
+uint16_t hdc1010::readData(uint8_t pointer)
+{
+	writeData(pointer) ;
+	uint8_t buf1 , buf0 ;
+	read ( i2cdevbus , &buf1 , 1 ) ;
+	read ( i2cdevbus , &buf0 , 1 ) ;
+	return buf1 << 8 | buf0 ;
+}
+
+void hdc1010::writeData(uint8_t val)
+{
+	uint8_t buf = val ;
+	write ( i2cdevbus , &buf , 1 ) ;
+	usleep ( 10000 ) ; //wait 10ms before release to give time for response
+}
+
+void hdc1010::readBytes(uint8_t * buf , uint8_t n )
+{
+	read(i2cdevbus,buf,n) ;
+	return ;
+}
