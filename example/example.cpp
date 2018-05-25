9c6b0271f2597e5a7f4482d9a09f48ebdc5e475c
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2018-05-25 14:47:25 -0400

    Example bugfix

--- example/example.cpp
+++ example/example.cpp
@@ -11,7 +11,7 @@ int main ( void )
 	cout << "Mf ID Dev2: 0x" << hex << dev2.readMfId()  << endl ;
 	uint64_t time = 0 ;
 	while(true)
-	{	cout << "At " << ++time << " seconds:" << endl ;
+	{	cout << "At " << dec << ++time << " seconds:" << endl ;
 		cout << "Temp Dev1: " << dev1.readT() << " C" << endl ;
 		cout << "Temp Dev2: " << dev2.readT() << " C" << endl ;
 		cout << endl ;
